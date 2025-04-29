c42dcb6741eae106efc1260df3a18ee927e68552
Aline Nunes <2001.alinenunes@gmail.com>
2025-04-29 16:35:23 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// script.js
-
 let encryptedText = '';
 
 function encryptText() {
     const input = document.getElementById('inputText').value;
     encryptedText = Array.from(input).map(char => String.fromCharCode(char.charCodeAt(0) + 1)).join('');
     document.getElementById('outputText').value = encryptedText;
-    document.getElementById('imagePlaceholder').style.display = 'none'; 
+    document.getElementById('imagePlaceholder').style.display = 'none';
+}
 
 function decryptText() {
     const input = document.getElementById('outputText').value;
     const decrypted = Array.from(input).map(char => String.fromCharCode(char.charCodeAt(0) - 1)).join('');
     document.getElementById('inputText').value = decrypted;
-    
+
     if (decrypted.trim() === '') {
         document.getElementById('imagePlaceholder').style.display = 'block';
     } else {
         document.getElementById('imagePlaceholder').style.display = 'none';
     }
 }
-
